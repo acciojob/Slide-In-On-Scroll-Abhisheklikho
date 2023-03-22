19d1fec7885a4de7cadaa088d4b5e4922ec8c7fb
Abhisheklikho <111559519+Abhisheklikho@users.noreply.github.com>
2023-03-22 13:31:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // Your JS code here.
+
+const slideImages = document.querySelectorAll('.slide-in');
+
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function() {
+    const context = this, args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+function checkSlide() {
+  slideImages.forEach(slideImage => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+    // bottom of the image
+    const imageBottom = slideImage.offsetTop + slideImage.height;
+    const isHalfShown = slideInAt > slideImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      slideImage.classList.add('active');
+    } else {
+      slideImage.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
